84fd6bb7bbec532ed59e322b5b51df20d927ccdb
yourunb <bxyura@gmail.com>
2025-04-15 23:19:16 +0300

    feat: add grid with btn

--- ADO.NET_forms/ADO.NET_forms/Form1.Designer.cs
+++ ADO.NET_forms/ADO.NET_forms/Form1.Designer.cs
@@ -29,12 +29,103 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.myDBDataSet = new ADO.NET_forms.MyDBDataSet();
+            this.t1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.t1TableAdapter = new ADO.NET_forms.MyDBDataSetTableAdapters.t1TableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.f1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.f2DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.f1DataGridViewTextBoxColumn,
+            this.f2DataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.t1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(210, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(349, 225);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // myDBDataSet
+            // 
+            this.myDBDataSet.DataSetName = "MyDBDataSet";
+            this.myDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // t1BindingSource
+            // 
+            this.t1BindingSource.DataMember = "t1";
+            this.t1BindingSource.DataSource = this.myDBDataSet;
+            // 
+            // t1TableAdapter
+            // 
+            this.t1TableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // f1DataGridViewTextBoxColumn
+            // 
+            this.f1DataGridViewTextBoxColumn.DataPropertyName = "f1";
+            this.f1DataGridViewTextBoxColumn.HeaderText = "f1";
+            this.f1DataGridViewTextBoxColumn.Name = "f1DataGridViewTextBoxColumn";
+            // 
+            // f2DataGridViewTextBoxColumn
+            // 
+            this.f2DataGridViewTextBoxColumn.DataPropertyName = "f2";
+            this.f2DataGridViewTextBoxColumn.HeaderText = "f2";
+            this.f2DataGridViewTextBoxColumn.Name = "f2DataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private MyDBDataSet myDBDataSet;
+        private System.Windows.Forms.BindingSource t1BindingSource;
+        private MyDBDataSetTableAdapters.t1TableAdapter t1TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn f1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn f2DataGridViewTextBoxColumn;
     }
 }
 
--- ADO.NET_forms/ADO.NET_forms/Form1.cs
+++ ADO.NET_forms/ADO.NET_forms/Form1.cs
@@ -16,5 +16,23 @@ namespace ADO.NET_forms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "myDBDataSet.t1". При необходимости она может быть перемещена или удалена.
+            this.t1TableAdapter.Fill(this.myDBDataSet.t1);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.dataGridView1.DataSource = this.myDBDataSet;
+            this.dataGridView1.DataMember = "t1";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
